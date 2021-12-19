7e4bb0aca95ffbb0228919d7cadeee61c28f69f8
Alpha_Bah <71989597+Alpha871@users.noreply.github.com>
2021-12-19 06:57:23 -0800

    Worked on My contry

---
+++ Mycountry/index.js
@@ -0,0 +1,11 @@
+
+
+
+document.addEventListener("click", function(e){
+  if(e.target.classList.contains("gallery-item")) {
+    const src = e.target.getAttribute("src");
+    document.querySelector(".modal-img").src = src;
+    var myModal = new bootstrap.Modal(document.getElementById('gallery-photo'));
+    myModal.show();
+  }
+});
